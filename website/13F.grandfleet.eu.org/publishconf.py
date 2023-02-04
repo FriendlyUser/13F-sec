9c745efb0b986072e18db8bb6d1cadc65f51856c
David Li <davidli012345@gmail.com>
2023-02-03 22:30:37 -0800

    Update publishconf.py

--- website/13F.grandfleet.eu.org/publishconf.py
+++ website/13F.grandfleet.eu.org/publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://13f.grandfleet.eu.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
